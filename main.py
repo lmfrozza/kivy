32ef12477ea7524b2eed9e19c90c6cfb24c25e25
lmfrozza <frozzalucca@gmail.com>
2023-09-13 16:12:57 -0300

    a

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ GUI = Builder.load_file("tela.kv")
 class MeuAplicativo(App):
     def build(self):
         return GUI
+    def on_button_press(self):
+        print("Button pressed!")
 
 MeuAplicativo().run()
