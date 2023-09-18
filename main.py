ab28ad2e148467299785dd4d44f4c237cc8d59a1
lmfrozza <frozzalucca@gmail.com>
2023-09-18 15:21:26 -0300

    it worked

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 import requests
 
 GUI = Builder.load_file("tela.kv")
 
-class MyApp(App):
+
+class Webscr(App):
     def build(self):
         return GUI
-MyApp().run()
-a = 1
+Webscr().run()
